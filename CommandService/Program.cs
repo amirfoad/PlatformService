30aff50260ede58d4179b69f6a7f3a149459e1d6
amirfoad ahmadi <amirfoadahmadi@gmail.com>
2026-04-20 22:52:51 +0330

    Add CommandService ASP.NET Core Web API project
    Scaffolded new CommandService microservice with .NET 10, including initial project file, controller, and configuration. Added OpenAPI/Scalar API UI, logging settings, launch profiles, and HTTP test file. Updated solution to include the new project.

---
+++ CommandService/Program.cs
@@ -0,0 +1,39 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+//builder.Services.AddAutoMapper(opt =>
+//{
+//    opt.AddProfile<CommandProfile>();
+//},
+//typeof(Program).Assembly);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.MapOpenApi();
+app.MapScalarApiReference(opt =>
+{
+    opt.Title = "Command Service API";
+    opt.Theme = ScalarTheme.Purple;
+    opt.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+});
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
